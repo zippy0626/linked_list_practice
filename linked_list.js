c242cf46b0565eddcc8142cf2e6a1b3425f4f324
zippy0626 <zippy0626@gmail.com>
2025-03-19 13:02:17 -0400

    added prepend and size methods

--- linked_list.js
+++ linked_list.js
@@ -2,14 +2,14 @@ import Node from "./linked_list_node.js";
 
 class LinkedList {
   constructor() {
-    //init a head and tail
+    // this is where we `store` the actual values
     this.head = null;
     this.tail = null;
   }
 
+  // `this` refers to the classes' instance's context
   append(value) {
     const node = new Node(value);
-
     if (!this.head) {
       this.head = node;
       this.tail = node;
@@ -19,10 +19,33 @@ class LinkedList {
     }
   }
 
-  prepend() {}
+  prepend(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.nextNode = this.head;
+      this.head = node;
+    }
+  }
+
+  size() {
+    if (!this.head || !this.tail) {
+      return 0;
+    }
+    let count = 0;
+    let currentNode = this.head;
+
+    while (currentNode !== null) {
+      currentNode = currentNode.nextNode
+      count += 1;
+    }
+
+    return count;
+  }
+
+
 }
 
 export default LinkedList;
-
-const linkedList = new LinkedList()
-console.log(linkedList.head, linkedList.tail)
